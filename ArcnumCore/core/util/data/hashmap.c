e97397a91b2598620c262dc53553fb8a3a5495c8
Chase Sunstrom <Casunstrom+github@gmail.com>
2024-01-03 19:26:22 -0600

    changed hashing function

--- ArcnumCore/core/util/data/hashmap.c
+++ ArcnumCore/core/util/data/hashmap.c
@@ -20,43 +20,75 @@ static inline u64 fmix64(u64 k) {
 }
 
 u64 _hash64(byte* _data, u64 len) {
-    const u64 m = 0xc6a4a7935bd1e995ULL;
-    const int r = 37; // Reduced the rotation value for a different mixing
 
-    u64 h = len * m;
+    const byte* data = _data;
+    const u64 nblocks = len / 16;
 
-    const u64* data = (const u64*)_data;
-    const u64* end = data + (len / 8);
+    u64 h = 0xDEADBEEFDEADBEEFULL;
 
-    while (data != end) {
-        u64 k = *data++;
+    const u64 c1 = 0x87c37b91114253d5;
+    const u64 c2 = 0x4cf5ad432745937f;
 
-        k *= m;
-        k ^= k >> r;
-        k *= m;
+    const u64* blocks = (const u64*)(data);
 
-        h ^= k;
-        h *= m;
+    for (int i = 0; i < nblocks; i++) {
+        u64 k1 = getblock64(blocks, i * 2 + 0);
+        u64 k2 = getblock64(blocks, i * 2 + 1);
+
+        k1 *= c1; k1 = ROTL64(k1, 31); k1 *= c2; h ^= k1;
+
+        h = ROTL64(h, 27); h += h; h = h * 5 + 0x52dce729;
+
+        k2 *= c2; k2 = ROTL64(k2, 33); k2 *= c1; h ^= k2;
+
+        h = ROTL64(h, 31); h += h; h = h * 5 + 0x38495ab5;
     }
+    const byte* tail = (const byte*)(data + nblocks * 16);
 
-    const byte* tail = (const byte*)data;
+    u64 k1 = 0;
 
-    switch (len & 7) {
-    case 7: h ^= ((u64)tail[6]) << 48;
-    case 6: h ^= ((u64)tail[5]) << 40;
-    case 5: h ^= ((u64)tail[4]) << 32;
-    case 4: h ^= ((u64)tail[3]) << 24;
-    case 3: h ^= ((u64)tail[2]) << 16;
-    case 2: h ^= ((u64)tail[1]) << 8;
-    case 1: h ^= ((u64)tail[0]);
-        h *= m;
+    switch (len & 15) {
+    case 15: k1 ^= ((u64)tail[14]) << 48;
+    case 14: k1 ^= ((u64)tail[13]) << 40;
+    case 13: k1 ^= ((u64)tail[12]) << 32;
+    case 12: k1 ^= ((u64)tail[11]) << 24;
+    case 11: k1 ^= ((u64)tail[10]) << 16;
+    case 10: k1 ^= ((u64)tail[9]) << 8;
+    case  9: k1 ^= ((u64)tail[8]) << 0;
+
+        k1 *= c2;
+        k1 = ROTL64(k1, 33);
+        k1 *= c1;
+        h ^= k1;
+        break;
+
+    case  8: k1 ^= ((u64)tail[7]) << 56;
+    case  7: k1 ^= ((u64)tail[6]) << 48;
+    case  6: k1 ^= ((u64)tail[5]) << 40;
+    case  5: k1 ^= ((u64)tail[4]) << 32;
+    case  4: k1 ^= ((u64)tail[3]) << 24;
+    case  3: k1 ^= ((u64)tail[2]) << 16;
+    case  2: k1 ^= ((u64)tail[1]) << 8;
+    case  1: k1 ^= ((u64)tail[0]) << 0;
+
+        k1 *= c1;
+        k1 = ROTL64(k1, 31);
+        k1 *= c2;
+        h ^= k1;
+        break;
     };
 
-    h ^= h >> r;
-    h *= m;
-    h ^= h >> r;
+    h ^= len;
+
+    h += h;
+    h = fmix64(h);
+
+    h += h;
+    h = fmix64(h);
 
     return h;
+
+
 }
 
 u64* _hash128(byte* _data, u64 len) {
@@ -165,7 +197,7 @@ hashmap(generic, generic) _hashmap_new(entry* entries, u64 num_entries)
 	return hmap;
 }
 
-void hashmap_insert(hashmap(generic, generic) hmap, byte* key, byte* value)
+void hashmap_insert(hashmap(generic, generic) hmap, generic key, generic value)
 {
     u64 hash = hash64(key);
     u64 index = hash % hmap->capacity;
